fa0c8c3f9579b42768ac2f19c79243e7f8b7e3a5
Caio lima <caiohenrys50@gmail.com>
2024-01-05 14:30:58 -0300

    feat: add textarea  lists

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,6 +3,7 @@ import { auth } from "@/services/firebase";
 import { User, onAuthStateChanged } from "firebase/auth"
 import { useEffect, useState } from "react"
 import { useAuth } from "@/authMiddlewares";
+import Textearea from "@/components/textearea";
 
 
 export default function Dashboard() {
@@ -22,10 +23,31 @@ useEffect(() => {
   }, []);
 
   return (
-    <div className="flex items-center justify-center relative">
-      <span className="flex -mt-5 p-2 px-4 rounded-md bg-zinc-300
-          text-zinc-950">{user?.displayName}</span>
-          <p>next dashboard</p>
+    <div className="bg-zinc-950 w-full flex items-center justify-center text-zinc-200">
+    { user?.displayName && <main className="md:w-3/4 w-full  px-6 flex mt-10">
+        <section className="w-full">
+          <div>
+            <h1 className="text-2xl font-semibold">
+              Qual sua tarefa 
+              <span className="text-red-500">?</span> 
+            </h1>
+            <form>
+              <Textearea
+                placeholder="Digite qual sua tarefa..."
+              />
+              <div className="flex items-center gap-2">
+                <input className="checkbox" type="checkbox"  />
+                <label>Deixa tarefa publicada</label>
+              </div>
+              <button
+                className="w-full p-2 bg-blue-500 rounded-md mt-4 text-1xl font-medium"
+                type="submit">
+                Confirmar
+              </button>
+            </form>
+          </div>
+        </section>
+      </main> || <div>entre com sua conta google...</div>}
     </div>
   )
 }
---
+++ src/components/textearea/index.tsx
@@ -0,0 +1,7 @@
+import { HTMLProps } from "react";
+
+export default function Textearea({ ...rest }: HTMLProps<HTMLTextAreaElement>) {
+  return (
+    <textarea className="w-full h-40 outline-none p-4 resize-none mb-2 mt-2" {...rest}></textarea>
+  )
+}
